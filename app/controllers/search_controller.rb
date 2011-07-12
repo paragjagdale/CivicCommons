23cef3e31e6fc0c9b19b74b2b78c24b23b6e784b
Parag Jagdale <paragjagdale@gmail.com>
2011-07-12 15:25:53 -0400

    Got search controller to use the service to return search results

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -5,9 +5,12 @@ class SearchController < ApplicationController
     @issues = Array.new
     @community = Array.new
 
-    search = Sunspot.search(Conversation, Issue) do
-      keywords(params[:q])
-    end
+    search_service = SearchService.new
+    search = search_service.fetch_results params[:q], Conversation, Issue
+
+#    search = Sunspot.search(Conversation, Issue) do
+#      keywords(params[:q])
+#    end
 
     # divide the search results into their respective types
     search.each_hit_with_result do |hit, result|
