3b96daaff5c7113557ae99eb76d4676920a712a0
Parag Jagdale <paragjagdale@gmail.com>
2011-07-21 11:17:52 -0400

    Removed separation of context from search results page and made everything inline

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -6,19 +6,7 @@ class SearchController < ApplicationController
     @community = Array.new
 
     search_service = SearchService.new
-    search = search_service.fetch_results params[:q], Conversation, Issue, Person
+    @results = search_service.fetch_results params[:q], Conversation, Issue, Person
 
-    # divide the search results into their respective types
-    search.each do |result|
-      case result
-      when Issue
-        @issues << result
-      when Conversation
-        @conversations << result
-      when Person
-        @community << result
-      else
-      end
-    end 
   end
 end
